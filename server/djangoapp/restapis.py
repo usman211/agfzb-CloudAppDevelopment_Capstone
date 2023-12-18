2a82ad0fae8b8a2deaa67fe3fc1177414ba2ed65
usman211 <usman211@gmail.com>
2023-12-18 02:47:26 -0500

    Task NLU Completed

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,11 @@
 import requests
 import json
-from .models import CarDealer
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
+import os
 
 
 # Create a `get_request` to make HTTP GET requests
@@ -64,17 +68,7 @@ def get_dealers_from_cf(url, **kwargs):
 
     return results
 # more functions
-def get_dealer_by_id(url, dealer_id):
-    # Call get_request with the dealer_id param
-    json_result = get_request(url, dealer_id=dealer_id)
 
-    # Create a CarDealer object from response
-    dealer = json_result[0]
-    dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
-                           id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
-                           short_name=dealer["short_name"],
-                           st=dealer["st"], zip=dealer["zip"])
-    return dealer_obj
 
 def get_dealer_by_state(url, dealer_state):
     # Call get_request with the dealer_id param
@@ -89,7 +83,7 @@ def get_dealer_by_state(url, dealer_state):
     return dealer_obj
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-def get_dealer_by_id_from_cf(url, id, **kwargs):
+def get_dealer_by_id_from_cf(url, id):
     result = {}
     # Call get_request with a URL parameter
     json_result = get_request(url, id=id)
@@ -110,38 +104,85 @@ def get_dealer_by_id_from_cf(url, id, **kwargs):
 
 
 
+def get_dealer_by_id(url, id):
+    # Call get_request with the dealer_id param
+    json_result = get_request(url, dealer_id=id)
+
+    # Create a CarDealer object from response
+    dealer = json_result[0]
+    dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
+                           id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
+                           short_name=dealer["short_name"],
+                           st=dealer["st"], zip=dealer["zip"])
+    return dealer_obj
+
+
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
 def get_dealer_reviews_from_cf(url, **kwargs):
     results = []
-    json_result = get_request(url, dealerId=kwargs['dealer_id'])
+    id = kwargs.get("id")
+    if id:
+        json_result = get_request(url, id=id)
+    else:
+        json_result = get_request(url)
+    print('json_result RESTAPIS', json_result)
     if json_result:
-        for review in json_result:
+        reviews = json_result
+        for dealer_review in reviews:
             review_obj = DealerReview(
-                dealership=review['dealership'],
-                name=review['name'],
-                purchase=review['purchase'],
-                review=review['review'],
-                purchase_date=review['purchase_date'],
-                car_make=review['car_make'],
-                car_model=review['car_model'],
-                car_year=review['car_year'],
-                sentiment=analyze_review_sentiments(review['review']),
-                id=review['_id'],
+                dealership=dealer_review.get("dealership"),
+                name=dealer_review.get("name"),
+                purchase=dealer_review.get("purchase"),
+                review=dealer_review.get("review"),
+                purchase_date=dealer_review.get("purchase_date"),
+                car_make=dealer_review.get("car_make"),
+                car_model=dealer_review.get("car_model"),
+                car_year=dealer_review.get("car_year"),
+                sentiment='',
+                id=dealer_review.get("id")
             )
+            sentiment = analyze_review_sentiments(review_obj.review)
+            review_obj.sentiment = sentiment
             results.append(review_obj)
     return results
 
 
-def analyze_review_sentiments(dealerreview):
-    version = '2022-04-07'
-    authenticator = IAMAuthenticator(NLU_API_KEY)
-    natural_language_understanding = NaturalLanguageUnderstandingV1(version=version, authenticator=authenticator)
-    natural_language_understanding.set_service_url(NLU_URL)
-    response = natural_language_understanding.analyze(text=dealerreview, features=Features(sentiment=SentimentOptions(targets=[dealerreview]))).get_result()
-    label = response['sentiment']['document']['label']
-    return label
+def analyze_review_sentiments(dealer_review):
+    try:
+        # Attempt to retrieve API key and URL from environment variables
+        apikey = os.environ.get("APINLU")  # export APINLU="apikey_nlu" in terminal
+        url = os.environ.get("URLNLU")  # export URLNLU="url_nlu" in terminal
+
+        # Check if API key and URL are available
+        if not apikey:
+            raise ValueError("LwG_yYL2rVwuc5eWGz6RAH9dx_KeZStH-mncDTWOB4FU")
+        if not url:
+            raise ValueError("https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/235f9b08-229d-41de-9014-445ee9ea2a35")
+        
+        authenticator = IAMAuthenticator(apikey)
+        natural_language_understanding = NaturalLanguageUnderstandingV1(
+            version='2022-04-07',
+            authenticator=authenticator
+        )
+
+        natural_language_understanding.set_service_url(url)
+
+        response = natural_language_understanding.analyze(
+            text=dealer_review,
+            language='en',
+            features=Features(sentiment=SentimentOptions(targets=[dealer_review]))
+        ).get_result()
+
+        print(json.dumps(response, indent=2))
+
+        return response["sentiment"]["document"]["label"]
+
+    except Exception as e:
+        # Handle any exceptions that may occur
+        print(f"Error in analyze_review_sentiments: {e}")
+        return None
 
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-from .restapis import get_dealers_from_cf,get_dealer_by_id_from_cf
+from .restapis import get_dealers_from_cf,get_dealer_by_id,get_dealer_by_id_from_cf,get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -93,7 +93,7 @@ def registration_request(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
-        url = "https://usman211-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        url = "https://usman211-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
         
         # Get dealers from the URL
         context = {
@@ -103,19 +103,38 @@ def get_dealerships(request):
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-def get_dealer_details(request, dealer_id):
+def get_dealer_details(request,dealer_id):
      if request.method == "GET":
          context = {}
-         dealer_url = "https://usman211-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
-         dealer = get_dealers_from_cf(dealer_url, dealer_id=id)
+         dealer_url = "https://usman211-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+         dealer = get_dealer_by_id_from_cf(dealer_url, id = dealer_id)
          context['dealer'] = dealer
 
-        #  review_url = "http://kevinraguett-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
-         reviews = get_dealer_reviews_from_cf(review_url, id = id)
+         review_url = "https://usman211-5000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
+         reviews = get_dealer_reviews_from_cf(review_url, id = dealer_id)
          context["reviews"] = reviews
          if not context["reviews"] :
             messages.warning(request, "There are no reviews at the moment !!!")   
          return render(request, 'djangoapp/dealer_details.html', context)
+
+
+def get_dealer_details1(request, dealer_id):
+     if request.method == "GET":
+         context = {}
+         dealer_url = "https://usman211-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+         dealer = get_dealer_by_id(dealer_url, id=id)
+         context['dealer'] = dealer
+         if not context["dealer"] :
+            messages.warning(request, "There are no dealer at the moment !!!")   
+         return render(request, 'djangoapp/dealer_details.html', context)
+
+
+        #  review_url = "https://usman211-5000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
+        #  reviews = get_dealer_reviews_from_cf(review_url, id = id)
+        #  context["reviews"] = reviews
+        #  if not context["reviews"] :
+        #     messages.warning(request, "There are no reviews at the moment !!!")   
+        #  return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
